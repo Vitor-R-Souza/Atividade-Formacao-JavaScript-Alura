1d44342d247e0e9e3767c1541404699a7d6833b5
Vitor_R_De_Souza <101133891+Vitor-R-Souza@users.noreply.github.com>
2024-11-02 16:12:24 -0300

    novas atividades

---
+++ Variaveis-funcoes/A14.js
@@ -0,0 +1,10 @@
+const logado = true;
+const admin = true;
+
+const resultado = logado && admin;
+
+if(resultado){
+    console.log('Administrador está logado no sistema'); 
+}else{
+    console.log('Administrador não está logado no sistema');
+}
---
+++ Variaveis-funcoes/A15.js
@@ -0,0 +1,6 @@
+let cond1 = true;
+let cond2 = false;
+
+let resultado = cond1 || cond2;
+
+console.log('O resultado é: ' + resultado);
